7ff6acd97f9c4cfab38148fad6095b4040f6b59d
yyccbbz <juancan@163.com>
2016-08-26 16:58:39 +0800

    Merge branch 'master' of E:\workspace\intellij2016\big-client-new with conflicts.

--- src/main/java/org/blazer/bigclient/action/BcExternalUserController.java
+++ src/main/java/org/blazer/bigclient/action/BcExternalUserController.java
@@ -1,8 +1,12 @@
 package org.blazer.bigclient.action;
 
+import org.blazer.bigclient.model.BcExternalUser;
+import org.blazer.bigclient.service.BcExternalUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by cuican on 2016-8-26.
@@ -12,7 +16,13 @@ public class BcExternalUserController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BcExternalUserController.class);
 
+    @Autowired
+    private BcExternalUserService bcExternalUserService;
 
+    @RequestMapping()
+    public String toPage(){
 
+        return null;
+    }
 
 }
---
+++ src/main/java/org/blazer/bigclient/service/BcExternalUserService.java
@@ -0,0 +1,13 @@
+package org.blazer.bigclient.service;
+
+import org.blazer.bigclient.model.BcExternalUser;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by cuican on 2016-8-26.
+ */
+@Service
+public class BcExternalUserService extends BaseService<BcExternalUser> {
+
+
+}
