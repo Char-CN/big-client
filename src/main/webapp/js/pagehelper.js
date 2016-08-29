4cc09c16a6d79b96c047c2616303928c96229542
hyy <hyy@192.168.99.1>
2016-08-29 17:13:04 +0800

    1.更新分页控件

--- src/main/webapp/js/pagehelper.js
+++ src/main/webapp/js/pagehelper.js
@@ -6,10 +6,21 @@ var PageHelper = function(id) {
 		var showSize = 9;
 		pagination.empty();
 		var li_first = $('<li><span>首页</span></li>');
-		li_first.click(function() {
-			findByPage(1);
-		});
+		var li_up = $('<li><span>上一页</span></li>');
 		pagination.append(li_first);
+		pagination.append(li_up);
+		
+		if (currentPage == 1) {
+			li_first.addClass("disabled");
+			li_up.addClass("disabled");
+		} else {
+			li_first.click(function() {
+				findByPage(1);
+			});
+			li_up.click(function() {
+				findByPage(currentPage - 1);
+			});
+		}
 
 		var totalPage = Math.ceil(total / pageSize);
 		var start = 0;
@@ -36,11 +47,23 @@ var PageHelper = function(id) {
 			});
 			pagination.append(li);
 		}
+
+		var li_down = $('<li><span>下一页</span></li>');
 		var li_end = $('<li><span>尾页</span></li>');
-		li_end.click(function() {
-			findByPage(totalPage);
-		});
+		pagination.append(li_down);
 		pagination.append(li_end);
+
+		if (currentPage == totalPage) {
+			li_down.addClass("disabled");
+			li_end.addClass("disabled");
+		} else {
+			li_down.click(function() {
+				findByPage(currentPage + 1);
+			});
+			li_end.click(function() {
+				findByPage(totalPage);
+			});
+		}
 		pagination.append('<li><span>共' + total + '条数据</span></li>');
 	};
 };
