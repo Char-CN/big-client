9bbd5f1cbeaf84cc450d79de5c8057282ea8515c
yyccbbz <juancan@163.com>
2016-08-29 23:38:55 +0800

    更换返回的分页bean

--- src/main/java/org/blazer/bigclient/action/BcExternalUserController.java
+++ src/main/java/org/blazer/bigclient/action/BcExternalUserController.java
@@ -1,11 +1,6 @@
 package org.blazer.bigclient.action;
 
-import java.util.HashMap;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.blazer.bigclient.body.PageBody;
+import com.github.pagehelper.PageInfo;
 import org.blazer.bigclient.model.BcExternalUser;
 import org.blazer.bigclient.service.BcExternalUserService;
 import org.blazer.bigclient.util.IntegerUtil;
@@ -15,6 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 
 /**
  * Created by cuican on 2016-8-26.
@@ -30,7 +29,7 @@ public class BcExternalUserController extends BaseController {
 
 	@ResponseBody
 	@RequestMapping("findByPage")
-	public PageBody<BcExternalUser> listAllUserByPage(HttpServletRequest request, HttpServletResponse response) {
+	public PageInfo<BcExternalUser> listAllUserByPage(HttpServletRequest request, HttpServletResponse response) {
 		HashMap<String, String> params = getParamMap(request);
 		LOGGER.debug("currentPage:" + IntegerUtil.getInt0(params.get("currentPage")) + ", pageSize:" + IntegerUtil.getInt0(params.get("pageSize")));
 		return bcExternalUserService.findByPage(params);
--- src/main/java/org/blazer/bigclient/service/BcExternalUserService.java
+++ src/main/java/org/blazer/bigclient/service/BcExternalUserService.java
@@ -1,20 +1,16 @@
 package org.blazer.bigclient.service;
 
-import java.util.HashMap;
-import java.util.List;
-
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.apache.commons.lang3.StringUtils;
-import org.blazer.bigclient.body.PageBody;
 import org.blazer.bigclient.model.BcExternalUser;
 import org.blazer.bigclient.util.IntegerUtil;
 import org.blazer.bigclient.util.StringUtil;
 import org.springframework.stereotype.Service;
-
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
-
 import tk.mybatis.mapper.entity.Example;
-import tk.mybatis.mapper.entity.Example.Criteria;
+
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by cuican on 2016-8-26.
@@ -22,21 +18,16 @@ import tk.mybatis.mapper.entity.Example.Criteria;
 @Service
 public class BcExternalUserService extends BaseService<BcExternalUser> {
 
-	public PageBody<BcExternalUser> findByPage(HashMap<String, String> params) {
-		PageBody<BcExternalUser> pb = new PageBody<BcExternalUser>();
+	public PageInfo<BcExternalUser> findByPage(HashMap<String, String> params) {
 		Example example = new Example(BcExternalUser.class);
-		Criteria criteria = example.createCriteria();
+		Example.Criteria criteria = example.createCriteria();
 		String search = StringUtil.getStrEmpty(params.get("search"));
 		if (StringUtils.isNotEmpty(search)) {
 			criteria.andCondition("phone_number like '%" + search + "%'" + " or sys_name like '%" + search + "%'");
 		}
-		Page<BcExternalUser> page = PageHelper.startPage(IntegerUtil.getInt0(params.get("currentPage")), IntegerUtil.getInt0(params.get("pageSize")));
+		PageHelper.startPage(IntegerUtil.getInt0(params.get("currentPage")), IntegerUtil.getInt0(params.get("pageSize")));
 		List<BcExternalUser> list = selectByExample(example);
-		pb.setCurrentPage(page.getPageNum());
-		pb.setPageSize(page.getPageSize());
-		pb.setTotal(page.getTotal());
-		pb.setRst(list);
-		return pb;
+		return new PageInfo<BcExternalUser>(list);
 	}
 
 }
