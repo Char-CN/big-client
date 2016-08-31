a5afa1bc31892e418a661ff62bf55ab62646dee1
yyccbbz <juancan@163.com>
2016-08-31 10:13:08 +0800

    20160831

--- src/main/java/org/blazer/bigclient/util/StringUtil.java
+++ src/main/java/org/blazer/bigclient/util/StringUtil.java
@@ -1,11 +1,26 @@
 package org.blazer.bigclient.util;
 
+/**
+ * 字符串处理工具
+ */
 public class StringUtil {
 
+	/**
+	 * 校验为空串
+	 *
+	 * @param o
+	 * @return
+	 */
 	public static String getStrEmpty(Object o) {
 		return o == null ? "" : o.toString();
 	}
 
+	/**
+	 * 校验为null
+	 *
+	 * @param o
+	 * @return
+	 */
 	public static String getStr(Object o) {
 		return o == null ? null : o.toString();
 	}
