18f865dc8c13287c38a415be86845576795e45ef
yyccbbz <juancan@163.com>
2016-09-27 16:35:40 +0800

    优化 edit add delete 页面

--- src/main/java/org/blazer/bigclient/service/BcExternalUserService.java
+++ src/main/java/org/blazer/bigclient/service/BcExternalUserService.java
@@ -4,8 +4,10 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.apache.commons.lang3.StringUtils;
 import org.blazer.bigclient.model.BcExternalUser;
+import org.blazer.bigclient.model.BcExternalUserBackup;
 import org.blazer.bigclient.util.IntegerUtil;
 import org.blazer.bigclient.util.StringUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tk.mybatis.mapper.entity.Example;
 
@@ -17,6 +19,9 @@ import java.util.List;
  */
 @Service
 public class BcExternalUserService extends BaseService<BcExternalUser> {
+
+    @Autowired
+    private BcExternalUserBackupService bcExternalUserBackupService;
 
     /**
      * 分页条件查询用户列表
@@ -51,5 +56,45 @@ public class BcExternalUserService extends BaseService<BcExternalUser> {
         }
         List<BcExternalUser> list = selectByExample(example);
         return list;
+    }
+
+    /**
+     * 保存单个用户到两张表
+     *
+     * @param bcExternalUser
+     * @param bcExternalUserBackup
+     * @return
+     */
+    public int saveUserToTwo(BcExternalUser bcExternalUser, BcExternalUserBackup bcExternalUserBackup) {
+        int num1 = this.save(bcExternalUser);
+        int num2 = this.bcExternalUserBackupService.save(bcExternalUserBackup);
+        if (num1 > 0 && num2 > 0) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * 查找手机号是否存在
+     * 两张表里有一张存在就是true，都不存在即为false
+     *
+     * @param phoneNumber
+     * @return
+     */
+    public Boolean findByPhoneNumber(String phoneNumber) {
+        //构建查询条件
+        BcExternalUser bcExternalUser = new BcExternalUser();
+        bcExternalUser.setPhoneNumber(Long.parseLong(phoneNumber));
+        BcExternalUserBackup bcExternalUserBackup = new BcExternalUserBackup();
+        bcExternalUserBackup.setPhoneNumber(bcExternalUser.getPhoneNumber());
+        //查询
+        BcExternalUser externalUser = this.mapper.selectOne(bcExternalUser);
+        BcExternalUserBackup externalUserBackup = this.bcExternalUserBackupService.mapper.selectOne(bcExternalUserBackup);
+        if (externalUser == null && externalUserBackup == null) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
---
+++ src/test/java/org/blazer/bigclient/test/BcExternalUserMapperTest.java
@@ -0,0 +1,80 @@
+package org.blazer.bigclient.test;
+
+import org.apache.ibatis.session.SqlSession;
+import org.blazer.bigclient.mapper.BcExternalUserMapper;
+import org.blazer.bigclient.model.BcExternalUser;
+import org.blazer.bigclient.util.DateUtil;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * Created by cuican on 2016-9-23.
+ */
+public class BcExternalUserMapperTest extends BasicTest {
+
+    @Autowired
+    private SqlSession sqlSession;
+
+    @Test
+    public void addUserTest() {
+        //获取当前mapper
+        BcExternalUserMapper userMapper = sqlSession.getMapper(BcExternalUserMapper.class);
+        //批量添加用户
+        for (int i = 0; i < 100; i++) {
+            BcExternalUser user = new BcExternalUser();
+            if(i % 2 == 0){
+                user.setPhoneNumber(18899990000L + i);
+                user.setSysName("刘备"+i);
+                user.setSysIfRegister("是");
+                user.setSysRegisterDate(DateUtil.thisDate());
+                user.setSysIfRealName("是");
+                user.setSysIfBindCard("是");
+                user.setSysIfTransaction("是");
+                user.setSysReferrer("张飞"+i);
+                user.setSysRebateExpirationDate(DateUtil.thisDate());
+            }else {
+                user.setPhoneNumber(16699990000L + i);
+                user.setSysName("关羽"+i);
+                user.setSysIfRegister("否");
+                user.setSysRegisterDate(DateUtil.thisDate());
+                user.setSysIfRealName("否");
+                user.setSysIfBindCard("否");
+                user.setSysIfTransaction("否");
+                user.setSysReferrer("赵云"+i);
+                user.setSysRebateExpirationDate(DateUtil.thisDate());
+            }
+//            String s = "";
+//            Random random = new Random();
+//            s += random.nextInt(9) + 1;
+//            for (int j = 0; j < 18 - 1; j++) {
+//                s += random.nextInt(10);
+//            }
+            user.setMtime(DateUtil.getFirstDateOfMonth(new Date()));
+            user.setCtime(user.getMtime());
+            userMapper.insert(user);
+        }
+
+
+    }
+
+    /**
+     * 产生18位的随机数
+     * @param a
+     */
+    public static void main(String[] a) {
+        String s = "";
+        Random random = new Random();
+        s += random.nextInt(9) + 1;
+        for (int i = 0; i < 18 - 1; i++) {
+            s += random.nextInt(10);
+            System.out.println("s = " + s);
+        }
+        BigInteger bigInteger = new BigInteger(s);
+        System.out.println(bigInteger);
+    }
+
+}
