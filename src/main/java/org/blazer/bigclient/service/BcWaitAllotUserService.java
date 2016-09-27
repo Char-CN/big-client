ec60b1f236dc3a1981e7993afc20b33d27711432
yyccbbz <juancan@163.com>
2016-09-28 00:37:56 +0800

    add waitallotuser 功能模块

---
+++ src/main/java/org/blazer/bigclient/service/BcWaitAllotUserService.java
@@ -0,0 +1,96 @@
+package org.blazer.bigclient.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.apache.commons.lang3.StringUtils;
+import org.blazer.bigclient.model.BcWaitAllotUser;
+import org.blazer.bigclient.model.BcWaitAllotUser;
+import org.blazer.bigclient.util.IntegerUtil;
+import org.blazer.bigclient.util.StringUtil;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by yyccb on 2016-9-27.
+ */
+@Service
+public class BcWaitAllotUserService extends BaseService<BcWaitAllotUser> {
+
+    /**
+     * 分页条件查询用户列表
+     *
+     * @param params
+     * @return
+     */
+    public PageInfo<BcWaitAllotUser> findByPage(HashMap<String, String> params) {
+        Example example = new Example(BcWaitAllotUser.class);
+        Example.Criteria criteria = example.createCriteria();
+        String search = StringUtil.getStrEmpty(params.get("search"));
+        if (StringUtils.isNotEmpty(search)) {
+            criteria.andCondition("phone_number like '%" + search + "%'" + " or user_name like '%" + search + "%'");
+        }
+        PageHelper.startPage(IntegerUtil.getIntZero(params.get("currentPage")), IntegerUtil.getIntZero(params.get("pageSize")));
+        List<BcWaitAllotUser> list = selectByExample(example);
+        return new PageInfo<BcWaitAllotUser>(list);
+    }
+
+    /**
+     * 根据条件查询外部拓展用户集合，
+     * 用于前台页面的Excel导出
+     *
+     * @param search
+     * @return
+     */
+    public List<BcWaitAllotUser> findBySearch(String search) {
+        Example example = new Example(BcWaitAllotUser.class);
+        Example.Criteria criteria = example.createCriteria();
+        if (StringUtils.isNotEmpty(search)) {
+            criteria.andCondition("phone_number like '%" + search + "%'" + " or user_name like '%" + search + "%'");
+        }
+        List<BcWaitAllotUser> list = selectByExample(example);
+        return list;
+    }
+//
+//    /**
+//     * 保存单个用户到两张表
+//     *
+//     * @param BcWaitAllotUser
+//     * @param BcWaitAllotUserBackup
+//     * @return
+//     */
+//    public int saveUserToTwo(BcWaitAllotUser BcWaitAllotUser, BcWaitAllotUserBackup BcWaitAllotUserBackup) {
+//        int num1 = this.save(BcWaitAllotUser);
+//        int num2 = this.BcWaitAllotUserBackupService.save(BcWaitAllotUserBackup);
+//        if (num1 > 0 && num2 > 0) {
+//            return 1;
+//        } else {
+//            return -1;
+//        }
+//    }
+//
+//    /**
+//     * 查找手机号是否存在
+//     * 两张表里有一张存在就是true，都不存在即为false
+//     *
+//     * @param phoneNumber
+//     * @return
+//     */
+//    public Boolean findByPhoneNumber(String phoneNumber) {
+//        //构建查询条件
+//        BcWaitAllotUser BcWaitAllotUser = new BcWaitAllotUser();
+//        BcWaitAllotUser.setPhoneNumber(Long.parseLong(phoneNumber));
+//        BcWaitAllotUserBackup BcWaitAllotUserBackup = new BcWaitAllotUserBackup();
+//        BcWaitAllotUserBackup.setPhoneNumber(BcWaitAllotUser.getPhoneNumber());
+//        //查询
+//        BcWaitAllotUser externalUser = this.mapper.selectOne(BcWaitAllotUser);
+//        BcWaitAllotUserBackup externalUserBackup = this.BcWaitAllotUserBackupService.selcetOne(BcWaitAllotUserBackup);
+//        if (externalUser == null && externalUserBackup == null) {
+//            return false;
+//        } else {
+//            return true;
+//        }
+//    }
+}
